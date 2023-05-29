6745220c0757055f2badb3f44710c46f0ec3d207
Leonardo Vini <leonardo.costa3@estudante.ifms.edu.br>
2023-05-29 13:57:58 -0400

    Primeiro Commit

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://hbbjcfqm:YKWkX4EdrerSIOYmjLSpPv-BR5--ivNQ@silly.db.elephantsql.com/hbbjcfqm'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
