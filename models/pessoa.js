3b19aeea8953025e9450d70d4bcc80d5c5cf389e
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-06-05 14:11:07 -0400

    Update pessoa.js

--- models/pessoa.js
+++ models/pessoa.js
@@ -15,7 +15,7 @@ class Pessoa {
   static async insert(data) {
     try {
       const connect = await db.connect();
-      const sql = "";
+      const sql = "INSERT INTO pessoas  VALUES('Leo V', 15, 'MS') ";
       const values = [data.nome, data.idade, data.uf];
       return await connect.query(sql, values);
     } catch (error) {
